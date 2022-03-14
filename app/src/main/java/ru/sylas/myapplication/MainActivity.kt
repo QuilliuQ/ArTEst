c8b859c491ee0e9aed20ac35e238a5e4683b62c8
sylas <sylas>
2022-03-14 20:02:08 +0500

    Initial commit

---
+++ app/src/main/java/ru/sylas/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package ru.sylas.myapplication
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
+import com.google.ar.core.Anchor
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var lampPostRenderable : ModelRenderable
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val arFragment: ArFragment = supportFragmentManager.findFragmentById(R.id.fragment) as ArFragment
+
+        ModelRenderable.builder()
+            .setSource(this, Uri.parse("sampledata/clock.fbx"))
+            .build()
+            .thenAccept { renderable: ModelRenderable ->
+                lampPostRenderable = renderable
+            }
+
+
+        arFragment.setOnTapArPlaneListener { hitresult: HitResult, _: Plane?, _: MotionEvent? ->
+            val anchor: Anchor = hitresult.createAnchor()
+            val anchorNode = AnchorNode(anchor)
+            anchorNode.setParent(arFragment.arSceneView.scene)
+            val lamp =
+                TransformableNode(arFragment.transformationSystem)
+            lamp.setParent(anchorNode)
+            lamp.renderable = lampPostRenderable
+            lamp.select()
+        }
+    }
+}
